31bb40e67887fcce1211913bbb38e048164a41a5
yabetsu93 <jabpau93@gmail.com>
2021-07-16 10:14:18 +0900

    generating for linux arm64 only and windows

--- make.sh
+++ make.sh
@@ -3,40 +3,7 @@
 # Reference:
 # https://github.com/golang/go/blob/master/src/go/build/syslist.go
 os_archs=(
-    aix/ppc64
-    darwin/386
-    darwin/amd64
-    dragonfly/amd64
-    freebsd/386
-    freebsd/amd64
-    freebsd/arm
-    freebsd/arm64
-    illumos/amd64
-    js/wasm
-    linux/386
     linux/amd64
-    linux/arm
-    linux/arm64
-    linux/ppc64
-    linux/ppc64le
-    linux/mips
-    linux/mipsle
-    linux/mips64
-    linux/mips64le
-    linux/riscv64
-    linux/s390x
-    netbsd/386
-    netbsd/amd64
-    netbsd/arm
-    netbsd/arm64
-    openbsd/386
-    openbsd/amd64
-    openbsd/arm
-    openbsd/arm64
-    plan9/386
-    plan9/amd64
-    plan9/arm
-    solaris/amd64
     windows/386
     windows/amd64
     windows/arm
